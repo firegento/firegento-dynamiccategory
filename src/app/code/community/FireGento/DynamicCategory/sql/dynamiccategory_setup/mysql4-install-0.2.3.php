e2a2e3f1e11bccc97ee03f1b990f3620c374802d
David Fuhr <fuhr@flagbit.de>
2011-05-15 22:04:27 +0200

    added install script to install table dynamiccategories_rule

---
+++ src/app/code/community/FireGento/DynamicCategory/sql/dynamiccategory_setup/mysql4-install-0.2.3.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @category   FireGento
+ * @package    FireGento_DynamicCategory
+ * @copyright  Copyright (c) 2011 FireGento Team (http://www.firegento.de)
+ */
+
+/** @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->run("
+-- DROP TABLE IF EXISTS {$this->getTable('dynamiccategory/rule')};
+CREATE TABLE IF NOT EXISTS {$this->getTable('dynamiccategory/rule')} (
+  `rule_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `conditions_serialized` MEDIUMTEXT NOT NULL,
+  PRIMARY KEY  (`rule_id`)
+) ENGINE=InnoDB;
+");
+
+$installer->endSetup();
